62d5b7a43f2ad6563a0ee7e63c45fafd61cb0a23
Md Jubair Ahmed <mjahmed.wd@gmail.com>
2021-04-17 01:32:49 +0600

    order status update added

--- src/components/Admin/Admin-ManageAllOrders/ManageSingleOrderItem/UpdateOrderStatus.js
+++ src/components/Admin/Admin-ManageAllOrders/ManageSingleOrderItem/UpdateOrderStatus.js
@@ -7,41 +7,39 @@ const UpdateOrderStatus = ({ order, handleClose }) => {
   const [productChangeStatus, setProductChangeStatus] = useContext(
     AllPlacedOrderContext
   );
-//   const { _id, name, price, address, country, description } = order;
+    const { _id, name, price, address, country, description } = order;
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => {
-    const eventData = {
-      name: data.name,
-      price: data.price,
-      address: data.address,
-      country: data.country,
-      description: data.description,
-    };
-    // fetch(`http://localhost:5000/updateProperty/${_id}`, {
-    //   method: "PATCH",
-    //   headers: { "Content-Type": "application/json" },
-    //   body: JSON.stringify(eventData),
-    // })
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     handleClose();
-    //     setProductChangeStatus(!productChangeStatus)
-    //   });
+    console.log(data);
+   
+    fetch(`http://localhost:5000/updateOrderStatus/${_id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        handleClose();
+        setProductChangeStatus(!productChangeStatus)
+      });
   };
 
   return (
     <>
       <div>
         <form onSubmit={handleSubmit(onSubmit)} className="row">
-          
           <div className="col-md-6">
             <h6>Property Description</h6>
-            <input
-            //   defaultValue={description}
-              className="w-75"
-              placeholder="Property Description"
-              {...register("description", { required: true })}
-            />
+            <select {...register("status")}>
+              <option value="Processing">Processing</option>
+              <option value="Confirmed">Confirmed</option>
+              <option value="Checked In">Checked In</option>
+              <option value="Checked Out">Checked Out</option>
+              <option value="Order Completed">Order Completed</option>
+              <option value="Canceled">Canceled</option>
+              <option value="Refund Processing">Refund Processing</option>
+              <option value="Refunded">Refunded</option>
+            </select>
           </div>
 
           <div className="col-md-6 button-wrap">
